3c3d2bf4930da326c819280a39be81022298f081
William Gaul <willyg302@gmail.com>
2014-05-07 21:35:45 -1000

    New strap.py setup

---
+++ strapme.py
@@ -0,0 +1,27 @@
+config = {
+	'project': 'jarvis',
+	'tasks': {
+		'install_kernel': {
+			'name': 'Install Kernel',
+			'root': 'kernel',
+			'virtualenv': 'env',
+			'run': ['pip install sockjs-tornado'],
+			'freeze': 'requirements.txt'
+		},
+		'npm_root': {
+			'name': 'NPM root',
+			'run': ['npm install']
+		},
+		'npm_nw': {
+			'name': 'NPM node-webkit app',
+			'root': 'jarvis-nw',
+			'run': ['npm install']
+		},
+		'install': {
+			'run': ['install_kernel', 'npm_root', 'npm_nw']
+		},
+		'default': {
+			'run': ['install']
+		}
+	}
+}
